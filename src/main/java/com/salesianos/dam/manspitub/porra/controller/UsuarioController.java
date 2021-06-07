8b47205dad48beb4ae505bc4aa780854e6dac499
Manuel Spinola Tubio <spinola.tuman21@triana.salesianos.edu>
2021-06-07 23:35:22 +0200

    hecho th:usuario y empezado con usuaricontroller

---
+++ src/main/java/com/salesianos/dam/manspitub/porra/controller/UsuarioController.java
@@ -0,0 +1,34 @@
+package com.salesianos.dam.manspitub.porra.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.salesianos.dam.manspitub.porra.model.Usuario;
+
+@Controller
+public class UsuarioController {
+	
+	
+	
+	
+	@GetMapping("/usuario")
+	public String index(Model model) {
+		model.addAttribute("productos")
+	}
+	
+	@GetMapping("/usuario/nuevo")
+	public String nuevoUsuario(Model model) {
+		model.addAttribute("usuario", new Usuario());
+		
+		model.addAttribute("porras")
+		return "form-usuario";
+	}
+	
+	@PostMapping("/usuario/nuevo/submit")
+	public String submitNuevoUsuario(Usuario usuario, Model model) {
+		return "redirect:/usuario/";
+	}
+}
